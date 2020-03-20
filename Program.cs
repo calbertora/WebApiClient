8dfc3dda068486fda3ac744afdff28d3553a3e45
Carlos Ramirez <calbertora@gmail.com>
2020-03-19 20:30:28 -0500

    Add the async get to the Github API

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace WebApiClient
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task ProcessRepositoriesAsync()
         {
-            Console.WriteLine("Hello World!");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")
+            );
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.WriteLine(msg);
+        }
+        
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositoriesAsync();
         }
     }
 }
